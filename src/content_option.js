7a152b2d0e9ba2da7712622aff6bb41a137b8028
Nisha Singh <coder.nishasingh@gmail.com>
2022-06-30 12:11:30 +0530

    Update portfolio

---
+++ src/content_option.js
@@ -0,0 +1,167 @@
+const logotext = "NS";
+const meta = {
+    title: "Nisha Singh",
+    description: "I’m Nisha Singh Full Stack devloper,currently working in Trivialworks Solutions PVT. LTD",
+};
+
+const introdata = {
+    title: "I’m Nisha Singh",
+    animated: {
+        first: "I love coding",
+        second: "I code cool websites",
+        third: "I developed web application",
+    },
+    description: `<p>I've started my career with <strong>PHP</strong> from WAPFLUID Technology PVT. LTD.<br />After 1.6 years, I've started work with <strong>NODE JS & React JS</strong> as a <strong>FULL Stack Web Developer</strong> From Trivialworks Solutions PVT. LTD.<br /><br /></p>`,
+    your_img_url: "https://raw.githubusercontent.com/codernisha/portfolio/master/src/assets/Images/profile-img.png"
+    //your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+    title: "About my self",
+    aboutme: "<p>I've started my career with <strong>PHP</strong> from WAPFLUID Technology PVT. LTD.<br />After 1.6 years, I've started work with <strong>NODE JS & React JS</strong> as a <strong>FULL Stack Web Developer</strong> From Trivialworks Solutions PVT. LTD.<br /><br /></p>",
+};
+const worktimeline = [
+    {
+        jobtitle: "Web Developer",
+        where: "Wapfluid Technology",
+        date: "12 MAY 2018 - 10 OCT 2019",
+    },
+    {
+        jobtitle: "Full Stack Developer",
+        where: "Trivialworks Solutions Pvt. LTD",
+        date: "27 JAN 2020 - 26 JUN 2022",
+    },
+];
+
+const skills = [{
+        name: "PHP",
+        value: 70,
+    },
+    {
+        name: "LARAVEL",
+        value: 60,
+    },
+    {
+        name: "Javascript",
+        value: 80,
+    },
+    {
+        name: "Node",
+        value: 80,
+    },
+    {
+        name: "React",
+        value: 85,
+    },
+    {
+        name: "MySQL",
+        value: 90,
+    },
+];
+
+const services = [{
+        title: "Social Login",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+    {
+        title: "Payment Gateway",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+    {
+        title: "Video Streaming",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://picsum.photos/400/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/800/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/550/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "nishantmar1998@gmail.com",
+    YOUR_FONE: "+91 8115352152",
+    description: "I’m Nisha Singh Full Stack devloper,currently working in Trivialworks Solutions PVT. LTD",
+    YOUR_SERVICE_ID: "service_gm004he",
+    YOUR_TEMPLATE_ID: "template_240sqgd",
+    YOUR_USER_ID: "fzVQcvt945aFf8Aro",
+};
+
+const socialprofils = {
+    github: "https://github.com/codernisha/",
+    //facebook: "https://facebook.com",
+    linkedin: "https://www.linkedin.com/in/nishantmar1998/",
+    //twitter: "https://twitter.com",
+    //youtube: "https://youtube.com",
+    //twitch: "https://twitch.com",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
